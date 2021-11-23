ce164aa51db3be21d7439176b4578b449a621ae6
Kaleb Vining <Kaleb.Vining@sckans.edu>
2021-11-23 12:03:24 -0600

    edit

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+import Picture from './thumbnail_Jinx.jpeg';
+
+const Hero = () => {
+    return(
+        <>
+            <img src={Picture} className="Jinx" />
+        </>
+    );
+};
+
+export default Hero;
+
